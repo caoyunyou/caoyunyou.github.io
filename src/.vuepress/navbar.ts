89a56bb14f06ebe85f3d6fefc46ce6be32b0d379
曹云友 <caoyunyou@foxmail.com>
2024-05-16 00:06:28 +0800

    小改动

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,10 +2,10 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/demo/",
   {
     text:"学习笔记",
     icon:"book",
     link:"/notes/"
-  }
+  },
+  "/demo/"
 ]);
